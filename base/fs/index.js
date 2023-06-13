d3e07396226506ac61f22b6bb9cfe97639f6f484
liushasha0012 <747043177@qq.com>
2023-06-13 23:13:21 +0800

    feat: 流式写入

--- base/fs/index.js
+++ base/fs/index.js
@@ -8,3 +8,10 @@ fs.writeFile('./base/fs/座右铭.txt', `明天又要上 b 班了 ---- ${new Dat
         console.log('写入成功！');
     }
 })
+
+// 流式写入 --- 适合写入频次高的场景
+const ws = fs.createWriteStream('./base/fs/座右铭.txt');
+ws.write('\r\n前端架构师');
+ws.write('\r\n基础建设与架构设计思想');
+
+ws.close();
